146803ed0d0e140d58b9975c8c7367389206a6f9
Srinath Rayabarapu <naath.r@gmail.com>
2023-03-08 16:47:00 +0530

    Initial commit

---
+++ src/main/java/com/springboot/grpc/SpringBootGRPCApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGRPCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGRPCApplication.class, args);
+	}
+
+}

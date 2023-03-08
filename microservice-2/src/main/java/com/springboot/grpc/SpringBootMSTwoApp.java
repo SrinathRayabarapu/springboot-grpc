067efb5e3d2b20095bb26cbd9dad6a102ea33154
Srinath Rayabarapu <naath.r@gmail.com>
2023-03-08 16:59:38 +0530

    Apps are having different ports to run

---
+++ microservice-2/src/main/java/com/springboot/grpc/SpringBootMSTwoApp.java
@@ -0,0 +1,13 @@
+package com.springboot.grpc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SpringBootMSTwoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMSTwoApp.class, args);
+    }
+}
